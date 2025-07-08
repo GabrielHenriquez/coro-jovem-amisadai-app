4c62d3faf5c345764bcebec5ad56d6f4d71645ad
Gabriel Henrique <95993363+GabrielHenriquez@users.noreply.github.com>
2025-07-08 11:27:02 -0300

    feat: add intro splash

---
+++ src/screens/intro/Splash/view.tsx
@@ -0,0 +1,25 @@
+import { Animated, View } from "react-native";
+import React, { useEffect, useRef } from "react";
+import Logo from "@assets/images/logo.svg";
+
+const SplashScreen = () => {
+  const opacity = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 2500,
+      useNativeDriver: true,
+    }).start();
+  }, [opacity]);
+
+  return (
+    <View className="flex-1 bg-primary justify-center items-center">
+      <Animated.View style={{ opacity }}>
+        <Logo width={280} height={280} />
+      </Animated.View>
+    </View>
+  );
+};
+
+export default SplashScreen;
