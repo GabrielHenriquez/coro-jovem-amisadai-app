39a2b6888d68c3148e019aadf046e40a5c85c61f
Gabriel Henrique <95993363+GabrielHenriquez@users.noreply.github.com>
2025-08-08 16:27:17 -0300

    feat: add auth navigation stack

--- src/features/auth/navigation/AuthStack.tsx
+++ src/features/auth/navigation/AuthStack.tsx
@@ -2,7 +2,7 @@ import {
   createNativeStackNavigator,
   NativeStackNavigationProp,
 } from "@react-navigation/native-stack";
-import AuthSPAScreen from "../screens/AuthSPAScreen";
+import AuthSPAScreen from "../presentation/screens/AuthSPAScreen";
 
 export type AuthStackParamList = {
   AuthSPA: undefined;
