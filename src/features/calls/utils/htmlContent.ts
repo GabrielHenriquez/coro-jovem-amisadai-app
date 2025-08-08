014e72729288371dbc91ef6843598ab5dbc0b5ce
Gabriel Henrique <95993363+GabrielHenriquez@users.noreply.github.com>
2025-08-08 16:42:15 -0300

    feat: data bind with eventData in htmlContent

--- src/features/calls/utils/htmlContent.ts
+++ src/features/calls/utils/htmlContent.ts
@@ -1,4 +1,8 @@
-const htmlContent = `<html lang="pt-BR">
+import { formatDateToBR } from "@features/members/utils/formatDate";
+import { IEvent } from "../domain/entities/Events";
+
+const htmlContent = (event: IEvent) => {
+  return `<html lang="pt-BR">
 
 <head>
     <meta charset="UTF-8">
@@ -117,17 +121,27 @@ const htmlContent = `<html lang="pt-BR">
             <div class="areaImg">
                 <img src="https://firebasestorage.googleapis.com/v0/b/coro-jovem-amisadai.appspot.com/o/logo.png?alt=media&token=0db0da4f-cc6a-4941-af04-f48e6367cab0">
             </div>
-            <h2>Chamada - Coro Jovem Amisadai (20/04/2025)</h2>
-            <p><strong>Tipo de chamada:</strong> Escala | <strong>Culto:</strong> Pregação</p>
+            <h2>Chamada - Coro Jovem Amisadai (${formatDateToBR(
+              event?.date
+            )})</h2>
+            <p><strong>Tipo de chamada:</strong> ${
+              event?.type
+            } | <strong>Culto:</strong> ${event?.cult}</p>
         </div>
 
         <div class="info-grid">
             <div><strong>Data:</strong> 20/04/2025</div>
-            <div><strong>Horário:</strong> 19:30H</div>
-            <div><strong>Escalado:</strong> Não informado</div>
-            <div><strong>Local:</strong> IEADPE - Casa Amarela</div>
-            <div><strong>Chamada por:</strong> GABRIEL HENRIQUE</div>
-            <div><strong>Músicas:</strong> SÓ O SENHOR É DEUS, DEUS DO MEU LOUVOR</div>
+            <div><strong>Horário:</strong> ${event?.hour}</div>
+            <div><strong>Escalado:</strong> ${
+              event?.namePreacher ?? "Não informado"
+            }</div>
+            <div><strong>Local:</strong> ${
+              event?.type === "Saída" && event?.local
+                ? event?.local
+                : "IEADPE - Casa Amarela"
+            }</div>
+            <div><strong>Chamada por:</strong> ${event?.scheduleBy}</div>
+            <div><strong>Músicas:</strong> ${event?.musics?.join(", ")}</div>
         </div>
 
 
@@ -137,66 +151,26 @@ const htmlContent = `<html lang="pt-BR">
 
             <div class="attendance-table">
                 <div class="table">
-                    <h3>Presentes (39)</h3>
+                    <h3>Presentes (${event?.components?.length})</h3>
                     <ul>
-                       <li><span>Edeilson Lucas</span> <span class="role">(Baixo)</span></li>
-                      <li><span>Edson Timóteo</span> <span class="role">(Baixo)</span></li>
-                      <li><span>Fabio Vicente</span> <span class="role">(1º Tenor)</span></li>
-                      <li><span>Jair Santana</span> <span class="role">(2º Tenor)</span></li>
-                      <li><span>Josué Calebe</span> <span class="role">(Baixo)</span></li>
-                      <li><span>Josué Ribeiro</span> <span class="role">(Baixo)</span></li>
-                      <li><span>João Vitor</span> <span class="role">(2º Tenor)</span></li>
-                      <li><span>Lucas Eberte</span> <span class="role">(Baixo)</span></li>
-                      <li><span>Manassés Franscisco</span> <span class="role">(1º Tenor)</span></li>
-                      <li><span>Matheus Nascimento</span> <span class="role">(1º Tenor)</span></li>
-                      <li><span>Nivaldo correia</span> <span class="role">(2º Tenor)</span></li>
-                      <li><span>Rafael Leite</span> <span class="role">(Baixo)</span></li>
-                      <li><span>Ryã Guilherme</span> <span class="role">(Barítono)</span></li>
-                      <li><span>Silvio Araujo</span> <span class="role">(Baixo)</span></li>
-                      <li><span>Vinícius Guilherme</span> <span class="role">(1º Tenor)</span></li>
-                      <li><span>Ana Carolina</span> <span class="role">(1º Soprano)</span></li>
-                      <li><span>Carla Carneiro</span> <span class="role">(1º Soprano)</span></li>
-                      <li><span>Carmem Lucia</span> <span class="role">(2º Soprano)</span></li>
-                      <li><span>Ellen caroline</span> <span class="role">(Contralto)</span></li>
-                      <li><span>Esther Silva Melo</span> <span class="role">(Contralto)</span></li>
-                      <li><span>Helena Gabrielle Gomes</span> <span class="role">(1º Soprano)</span></li>
-                      <li><span>Jamile Felix</span> <span class="role">(2º Soprano)</span></li>
-                      <li><span>Juliana Cecilia</span> <span class="role">(Contralto)</span></li>
-                      <li><span>Juliane Larissa</span> <span class="role">(1º Soprano)</span></li>
-                      <li><span>Kellyta Martins</span> <span class="role">(1º Soprano)</span></li>
-                      <li><span>Larissa Carla</span> <span class="role">(Contralto)</span></li>
-                      <li><span>Luiza Ester</span> <span class="role">(2º Soprano)</span></li>
-                      <li><span>Manuele Dias</span> <span class="role">(1º Soprano)</span></li>
-                      <li><span>Manuella souza</span> <span class="role">(Contralto)</span></li>
-                      <li><span>Mykaella Raissa</span> <span class="role">(Contralto)</span></li>
-                      <li><span>Ornela Berthiê</span> <span class="role">(1º Soprano)</span></li>
-                      <li><span>Rayssa Conceição</span> <span class="role">(Contralto)</span></li>
-                      <li><span>Stefania Silva Melo</span> <span class="role">(2º Soprano)</span></li>
-                      <li><span>Stefany Silva Melo</span> <span class="role">(Contralto)</span></li>
-                      <li><span>Talita Florentino</span> <span class="role">(1º Soprano)</span></li>
-                      <li><span>Tamandara da Silva</span> <span class="role">(Contralto)</span></li>
-                      <li><span>Thaiza Rodrigues</span> <span class="role">(Contralto)</span></li>
-                      <li><span>Thayane Florentino</span> <span class="role">(Contralto)</span></li>
-                      <li><span>Thays Florentino</span> <span class="role">(2º Soprano)</span></li>
+                        ${event?.components
+                          ?.map(
+                            (component) =>
+                              `<li><span>${component?.name}</span> <span class="role">(${component?.suit})</span></li>`
+                          )
+                          .join("")}
                     </ul>
                 </div>
 
                 <div class="table">
-                    <h3>Ausentes (13)</h3>
+                    <h3>Ausentes (${event?.missingComponents?.length})</h3>
                     <ul>
-                        <li><span>Diogo Vinícius</span> <span class="role">(1º Tenor)</span></li>
-                        <li><span>Emanuel Victor</span> <span class="role">(Baixo)</span></li>
-                        <li><span>Gabriel Henrique Soares</span> <span class="role">(2º Tenor)</span></li>
-                        <li><span>Jackson Avelino</span> <span class="role">(2º Tenor)</span></li>
-                        <li><span>Miguel Luiz</span> <span class="role">(Baixo)</span></li>
-                        <li><span>Pedro Augusto</span> <span class="role">(1º Tenor)</span></li>
-                        <li><span>Acsa Cristina</span> <span class="role">(2º Soprano)</span></li>
-                        <li><span>Ana Beatriz</span> <span class="role">(1º Soprano)</span></li>
-                        <li><span>Elaine Apolinário</span> <span class="role">(Contralto)</span></li>
-                        <li><span>Jamile Vitória Rosa da Silva</span> <span class="role">(2º Soprano)</span></li>
-                        <li><span>Lays Apolinário</span> <span class="role">(Contralto)</span></li>
-                        <li><span>Maria Júlia Muniz de Luna</span> <span class="role">(1º Soprano)</span></li>
-                        <li><span>Maria Vitória Edinger</span> <span class="role">(2º Soprano)</span></li>
+                        ${event?.missingComponents
+                          ?.map(
+                            (component) =>
+                              `<li><span>${component?.name}</span> <span class="role">(${component?.suit})</span></li>`
+                          )
+                          .join("")}
                     </ul>
                 </div>
             </div>
@@ -206,5 +180,6 @@ const htmlContent = `<html lang="pt-BR">
 
 </html>
 `;
+};
 
 export default htmlContent;
