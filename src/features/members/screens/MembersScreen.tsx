f92b9703238fb92926ed92ea73c527a537e7bb39
Gabriel Henrique <95993363+GabrielHenriquez@users.noreply.github.com>
2025-08-08 16:47:33 -0300

    feat: data bind in member Screen + search input fn

--- src/features/members/screens/MembersScreen.tsx
+++ src/features/members/screens/MembersScreen.tsx
@@ -1,18 +1,31 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { colors } from "@styles/colors";
-import { BottomSheetModal } from "@gorhom/bottom-sheet";
-import { useCallback, useRef } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 import { useFocusEffect } from "@react-navigation/native";
+import { useDebounce } from "use-debounce";
+import Toast from "@components/Toast/view";
 import MemberCard from "../components/MemberCard";
 import MemberPreview from "../components/MemberPreview";
+import useMembers from "../hooks/useMembers";
 import * as Component from "@components/index";
 import * as RN from "react-native";
+import { useQueryClient } from "@tanstack/react-query";
 
 const Members = () => {
-  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
   const scrollY = useRef(new RN.Animated.Value(0)).current;
+  const [searchTerm, setSearchTerm] = useState("");
+  const [debouncedSearchTerm] = useDebounce(searchTerm, 450);
   const flatListRef = useRef<RN.FlatList>(null);
   const scrollYRef = useRef(0);
+  const {
+    data,
+    handleGetMember,
+    bottomSheetModalRef,
+    memberSelected,
+    visibleToast,
+    setVisibleToast,
+    handleDeleteMember,
+  } = useMembers();
 
   const scrollToTop = () => {
     RN.Animated.timing(scrollY, {
@@ -24,13 +37,21 @@ const Members = () => {
     });
   };
 
-  const handlePresentModalPress = useCallback(() => {
-    bottomSheetModalRef.current?.present();
-  }, []);
-
   const handleScroll = (event: any) => {
     scrollYRef.current = event.nativeEvent.contentOffset.y;
   };
+
+  const filteredMembers = useMemo(() => {
+    if (!data) return [];
+
+    if (!debouncedSearchTerm?.trim()) return data;
+
+    const filter = data.filter((item) =>
+      item.name.toLowerCase().includes(debouncedSearchTerm.toLowerCase())
+    );
+
+    return filter;
+  }, [data, debouncedSearchTerm]);
 
   useFocusEffect(useCallback(() => scrollToTop(), []));
 
@@ -46,7 +67,7 @@ const Members = () => {
         }}
         className="flex-1 bg-background pt-4"
       >
-        <RN.View className="gap-4">
+        <RN.View className="gap-4 px-4">
           <Component.Text
             className="text-center font-poppinsBold text-primary"
             size={26}
@@ -56,23 +77,26 @@ const Members = () => {
 
           <Component.SearchInput
             styleRest={{ paddingHorizontal: 16 }}
-            searchTerm={""}
-            setSearchTerm={() => {}}
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
           />
 
           <Component.Text className="text-center font-poppinsSemiBold text-gray">
-            Quantidade de componentes: 54
+            Quantidade de componentes: {data?.length}
           </Component.Text>
         </RN.View>
 
         <Component.Spacer height={10} />
 
         <RN.FlatList
+          data={filteredMembers}
           ref={flatListRef}
           onScroll={handleScroll}
           scrollEventThrottle={16}
-          data={Array(54).fill(null)}
-          renderItem={() => <MemberCard onPress={handlePresentModalPress} />}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <MemberCard member={item} onPress={handleGetMember} />
+          )}
           contentContainerClassName="gap-2.5 pb-16 px-6 mt-2"
           keyboardShouldPersistTaps="handled"
           decelerationRate={0.87}
@@ -80,11 +104,21 @@ const Members = () => {
 
         <Component.BaseBottomSheet
           ref={bottomSheetModalRef}
-          indicatorColor="blue"
+          indicatorColor={
+            memberSelected?.gender === "Masculino" ? "blueGender" : "pinkGender"
+          }
         >
-          <MemberPreview />
+          <MemberPreview
+            memberPressed={memberSelected}
+            handleDeleteMember={handleDeleteMember}
+          />
         </Component.BaseBottomSheet>
       </RN.View>
+      <Toast
+        message="Componente excluído com sucesso!"
+        onHide={() => setVisibleToast(false)}
+        visible={visibleToast}
+      />
     </SafeAreaView>
   );
 };
