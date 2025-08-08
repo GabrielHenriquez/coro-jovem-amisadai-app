6ef3f0713c765e9b84a316c94d574869f24fc574
Gabriel Henrique <95993363+GabrielHenriquez@users.noreply.github.com>
2025-08-08 16:41:45 -0300

    feat: apply data bind and create fn to callScreen

--- src/features/calls/screens/CallsScreen.tsx
+++ src/features/calls/screens/CallsScreen.tsx
@@ -1,7 +1,6 @@
-import { useCallback, useRef, useState, memo, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Feather, MaterialIcons } from "@expo/vector-icons";
 import { htmlContent, ptBR } from "../utils/index";
-import { DayState } from "react-native-calendars/src/types";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {
   Text,
@@ -10,7 +9,6 @@ import {
   BaseBottomSheet,
 } from "@components/index";
 import { EventCard } from "../components";
-import { BottomSheetModal } from "@gorhom/bottom-sheet";
 import { colors } from "@styles/colors";
 import { Clock, Pen, Share2, Trash2 } from "lucide-react-native";
 import styles from "../styles/CallsStyles";
@@ -20,36 +18,59 @@ import * as RN from "react-native";
 import * as RNC from "react-native-calendars";
 import * as Print from "expo-print";
 import * as Share from "expo-sharing";
-import CalendarDay from "../components/CalendarDay";
+import { useCalls } from "../hooks/useCalls";
+import { EmptyCall } from "@assets/images";
+import Toast from "@components/Toast/view";
+import { useNavigation } from "@react-navigation/native";
+import { useBottomSheet } from "@gorhom/bottom-sheet";
 RNC.LocaleConfig.locales["pt-br"] = ptBR;
 RNC.LocaleConfig.defaultLocale = "pt-br";
 
 export default function CallScreen() {
   const [day, setDay] = useState<RNC.DateData>();
-  const [opennedCalendar, setOpennedCalendar] = useState(false);
-  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
-  const callPreviewModalRef = useRef<BottomSheetModal>(null);
+  const [currentMonthFormatted, setCurrentMonthFormatted] = useState("");
+
+  const { navigate } = useNavigation();
   const [pdfUri, setPdfUri] = useState<string | null>(null);
+  const [loadingEventPreview, setLoadingEventPreview] = useState(false);
+  const {
+    eventsBirthDate,
+    showToastDelete,
+    setShowToastDelete,
+    dots,
+    event,
+    handleModalAction,
+    getEventsByDateToCard,
+    events,
+    opennedCalendar,
+    toggleCalendar,
+    bottomSheetModalRef,
+    callPreviewModalRef,
+    validate,
+    currentDateSelected,
+    handleDeleteEvent,
+    isLoadingDelete,
+    handleGetEvent,
+  } = useCalls();
   const calendarHeight = opennedCalendar ? 355 : 150;
 
-  const handleModalAction = useCallback(
-    (ref: React.RefObject<any>, action: "present" | "close") => {
-      if (!ref.current) return;
-      if (action === "present") return ref.current.present();
-      ref.current.close();
-    },
-    []
-  );
+  const currentMonth = new Date()
+    .toLocaleDateString("pt-BR", { month: "long", year: "numeric" })
+    .replace(" de ", " ")
+    .replace(/^./, (letra) => letra.toUpperCase());
 
-  const generatePDF = async () => {
-    const { uri } = await Print.printToFileAsync({ html: htmlContent });
-    setPdfUri(uri);
+  const formatDateToBR = (date: string) => {
+    const newDate = new Date(date);
+    const formatted = newDate.toLocaleDateString("pt-BR");
+    return formatted;
   };
 
-  const validate = useCallback(() => {
-    handleModalAction(bottomSheetModalRef, "close");
-    setTimeout(() => handleModalAction(callPreviewModalRef, "present"), 500);
-  }, []);
+  const generatePDF = async () => {
+    setLoadingEventPreview(true);
+    const { uri } = await Print.printToFileAsync({ html: htmlContent(event) });
+    setPdfUri(uri);
+    setTimeout(() => setLoadingEventPreview(false), 300);
+  };
 
   useEffect(() => {
     if (pdfUri) validate();
@@ -59,14 +80,32 @@ export default function CallScreen() {
     await Share.shareAsync(pdfUri!);
   };
 
-  const toggleCalendar = useCallback((isOpen: boolean) => {
-    setOpennedCalendar(isOpen);
-  }, []);
+  const capitalize = (text: string) =>
+    text.charAt(0).toUpperCase() + text.slice(1);
+
+  const testData = () => {
+    const [ano, mes, dia] = currentDateSelected?.split("-").map(Number);
+    const data = new Date(ano, mes - 1, dia);
+
+    const formatador = new Intl.DateTimeFormat("pt-BR", {
+      weekday: "long",
+      day: "numeric",
+      month: "long",
+    });
+
+    let resultado = formatador?.format(data);
+    resultado = resultado.charAt(0).toUpperCase() + resultado.slice(1);
+    resultado = resultado.replace("-feira", "");
+
+    return resultado;
+  };
 
   const calendarHeader = (
     <RN.View style={styles.calendarHeader}>
       <Feather size={24} color="#E8E8E8" name="calendar" />
-      <RN.Text style={styles.calendarHeaderText}>Dezembro 2025</RN.Text>
+      <RN.Text style={styles.calendarHeaderText}>
+        {currentMonthFormatted ? currentMonthFormatted : currentMonth}
+      </RN.Text>
     </RN.View>
   );
 
@@ -86,17 +125,23 @@ export default function CallScreen() {
       arrowColor: "#E8E8E8",
       calendarBackground: "#4C5E46",
       textDayStyle: { color: "#E8E8E8" },
+      dotStyle: {
+        width: 8,
+        height: 8,
+        borderRadius: 50,
+      },
     },
     customHeaderTitle: calendarHeader,
     markedDates: day ? { [day.dateString]: { selected: true } } : undefined,
-    dayComponent: (props: { date?: RNC.DateData; state?: DayState }) => (
+    /*     dayComponent: (props: { date?: RNC.DateData; state?: DayState }) => (
       <CalendarDay
         date={props?.date!}
         state={props?.state!}
         day={day!}
         setDay={setDay}
+        test={getEventsByDateToCard}
       />
-    ),
+    ), */
   };
 
   return (
@@ -114,7 +159,19 @@ export default function CallScreen() {
             },
           ]}
         >
-          <RNC.CalendarProvider date={day?.dateString || "2025-07-07"}>
+          <RNC.CalendarProvider
+            date={currentDateSelected}
+            onMonthChange={(month) => {
+              const date = new Date(month.year, month.month - 1);
+              const raw = new Intl.DateTimeFormat("pt-BR", {
+                month: "long",
+                year: "numeric",
+              }).format(date);
+
+              const formatted = capitalize(raw.replace(" de ", " ")); // "Dezembro 2025"
+              setCurrentMonthFormatted(formatted);
+            }}
+          >
             <RN.View style={styles.calendarWrapper}>
               {/* ExpandableCalendar */}
               <RN.View
@@ -130,7 +187,12 @@ export default function CallScreen() {
                 <RNC.ExpandableCalendar
                   {...sharedCalendarProps}
                   onCalendarToggled={toggleCalendar}
+                  onDayPress={(day) => {
+                    getEventsByDateToCard(day?.dateString);
+                  }}
                   disablePan
+                  markingType="multi-dot"
+                  markedDates={dots}
                   disableWeekScroll
                   nestedScrollEnabled={false}
                   pastScrollRange={12}
@@ -153,8 +215,12 @@ export default function CallScreen() {
               >
                 <RNC.Calendar
                   {...sharedCalendarProps}
-                  minDate={new Date().toDateString()}
+                  onDayPress={({ dateString }) => {
+                    getEventsByDateToCard(dateString);
+                  }}
                   hideExtraDays
+                  markingType="multi-dot"
+                  markedDates={dots}
                   style={styles.calendar}
                 />
               </RN.View>
@@ -164,12 +230,7 @@ export default function CallScreen() {
 
         <RN.TouchableOpacity
           style={styles.toggleButton}
-          onPress={() =>
-            setOpennedCalendar((prev) => {
-              toggleCalendar(!prev);
-              return !prev;
-            })
-          }
+          onPress={toggleCalendar}
         >
           <Feather
             size={28}
@@ -180,22 +241,83 @@ export default function CallScreen() {
       </RN.View>
 
       <RN.View
-        style={{ paddingHorizontal: 16, marginTop: 12, marginBottom: 6 }}
+        style={{ paddingHorizontal: 16, marginTop: 14, marginBottom: 6 }}
       >
-        <Text size={20} className="font-poppinsSemiBold text-black">
-          Sexta, 27 de Dezembro
-        </Text>
+        {events?.length > 0 && (
+          <Text size={20} className="font-poppinsSemiBold text-black">
+            {testData()}
+          </Text>
+        )}
 
-        <EventCard
-          onPress={() => handleModalAction(bottomSheetModalRef, "present")}
-        />
+        <RN.View className="gap-1.5 mt-2">
+          <RN.View className="gap-0.5 ">
+            {events?.length > 0 ? (
+              events.map((event) => (
+                <EventCard
+                  key={event.id}
+                  event={event}
+                  onPress={() => handleGetEvent(event?.id)}
+                />
+              ))
+            ) : (
+              <RN.View className="items-center px-10">
+                <RN.Image
+                  source={EmptyCall}
+                  style={{
+                    width: 70,
+                    height: 70,
+                  }}
+                  resizeMode="contain"
+                />
+                <Text className="font-poppinsSemiBold text-center text-black">
+                  Nenhuma chamada registrada na {testData()}.
+                </Text>
+              </RN.View>
+            )}
+          </RN.View>
+
+          <RN.View className="gap-3 mt-2">
+            {eventsBirthDate.map((event) => (
+              <RN.View
+                style={{ borderWidth: 1, borderColor: "#CCC", elevation: 3 }}
+                className="px-4 py-2 bg-white rounded-xl gap-5 flex-row items-center"
+                key={event.id}
+              >
+                <RN.View
+                  style={{
+                    borderRadius: 30,
+                    width: 15,
+                    backgroundColor: "#B8BD2D",
+                  }}
+                  className="h-11"
+                />
+
+                <RN.View className="flex-row items-center gap-3">
+                  <RN.Image
+                    style={{ width: 44, height: 44, borderRadius: 22 }}
+                    source={{ uri: event?.profileImage }}
+                  />
+                  <RN.View className="gap-1">
+                    <Text className="font-poppinsSemiBold">
+                      Aniversariante do dia! 🎉
+                    </Text>
+                    <Text className="font-poppins">{event?.name}</Text>
+                  </RN.View>
+                </RN.View>
+              </RN.View>
+            ))}
+          </RN.View>
+        </RN.View>
       </RN.View>
 
-      <BaseBottomSheet ref={bottomSheetModalRef} indicatorColor={"redDark"}>
+      <BaseBottomSheet
+        ref={bottomSheetModalRef}
+        indicatorColor={event?.type === "Saída" ? "redEvent" : "blueEvent"}
+      >
         <RN.View className="items-center px-8">
           <RN.View className="gap-2 items-center mb-6 mt-2">
-            <Text size={20} className="font-poppinsSemiBold">
-              Chamada - CJA (28/04/2028)
+            <Text size={21} className="font-poppinsSemiBold">
+              Chamada - CJA ({formatDateToBR(event?.date!)})
             </Text>
 
             <RN.View
@@ -203,7 +325,7 @@ export default function CallScreen() {
             >
               <Feather size={18} name="book-open" color={colors.black} />
               <Text size={18} className="font-poppinsMedium text-grayDark">
-                Culto: Pregação
+                Culto: {event?.cult}
               </Text>
             </RN.View>
 
@@ -212,7 +334,7 @@ export default function CallScreen() {
             >
               <Clock size={18} color={colors.black} />
               <Text size={18} className="font-poppinsMedium  text-grayDark">
-                Horário: 19h30
+                Horário: {event?.hour}
               </Text>
             </RN.View>
           </RN.View>
@@ -220,6 +342,7 @@ export default function CallScreen() {
           <RN.View className="w-full gap-3.5">
             <Button
               styleRest={{ height: getHeight * 0.05 }}
+              activeLoading={loadingEventPreview}
               onPress={() => {
                 generatePDF();
               }}
@@ -236,6 +359,17 @@ export default function CallScreen() {
 
             <Button
               bgColor="background"
+              onPress={() => {
+                handleModalAction(bottomSheetModalRef, "close");
+                setTimeout(() => {
+                  navigate("CallsNavigation", {
+                    screen: "CreateCall",
+                    params: {
+                      event,
+                    },
+                  });
+                }, 400);
+              }}
               styleRest={{
                 height: getHeight * 0.05,
               }}
@@ -253,6 +387,8 @@ export default function CallScreen() {
                 borderColor: colors.redDark,
                 height: getHeight * 0.05,
               }}
+              activeLoading={isLoadingDelete}
+              onPress={() => handleDeleteEvent(event)}
             >
               <Trash2 strokeWidth={2.5} color={colors.redDark} size={18} />
               <Text size={14} className="font-poppinsSemiBold text-redDark">
@@ -272,7 +408,7 @@ export default function CallScreen() {
       >
         <RN.View style={{ height: 600 }}>
           <Text className="font-poppinsSemiBold text-center" size={20}>
-            Chamada - CJA (24/04/2025)
+            Chamada - CJA ({formatDateToBR(event?.date!)})
           </Text>
 
           {pdfUri && (
@@ -302,6 +438,12 @@ export default function CallScreen() {
           </Button>
         </RN.View>
       </BaseBottomSheet>
+
+      <Toast
+        message="Chamada excluída com sucesso!"
+        visible={showToastDelete}
+        onHide={() => setShowToastDelete(false)}
+      />
     </SafeAreaView>
   );
 }
