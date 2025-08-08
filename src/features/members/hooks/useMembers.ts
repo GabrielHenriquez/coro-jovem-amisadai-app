34e6a8f337ae565de5371733106a0ac4786f0433
Gabriel Henrique <95993363+GabrielHenriquez@users.noreply.github.com>
2025-08-08 16:31:55 -0300

    feat: add members hooks

---
+++ src/features/members/hooks/useMembers.ts
@@ -0,0 +1,66 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { FirebaseMembersRepository } from "../domain/repositories/FirebaseMembersRepository";
+import { MembersUseCase } from "../domain/usecases/MembersUseCase";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { IMember } from "../domain/entities/Member";
+import { BottomSheetModal } from "@gorhom/bottom-sheet";
+
+const membersRepo = new FirebaseMembersRepository();
+const membersUseCase = new MembersUseCase(membersRepo);
+
+const useMembers = () => {
+  const [memberSelected, setMemberSelected] = useState<IMember | null>(null);
+  const [visibleToast, setVisibleToast] = useState(false);
+
+  const queryClient = useQueryClient();
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+
+  const queryGetMembers = useQuery({
+    queryKey: ["members"],
+    queryFn: async () => {
+      const members = await membersUseCase.execute("getMembers");
+      console.log("✅ Membros obtidos com sucesso:", members.length);
+      return members;
+    },
+  });
+
+  const handleGetMember = async (id: string) => {
+    const member = await membersUseCase.execute("getMember", id);
+    console.log("✅ Membro mostrado com sucesso:", member?.name);
+    setMemberSelected(member);
+  };
+
+  const handleDeleteMember = async ({
+    id,
+    memberCard,
+    name,
+    profileImageUri,
+  }: IMember) => {
+    await membersRepo.deleteMember({ memberCard, name, id, profileImageUri });
+    bottomSheetModalRef.current?.close(), 1000;
+    queryClient.invalidateQueries({
+      queryKey: ["members"],
+    });
+    setVisibleToast(true);
+  };
+
+  useEffect(() => {
+    if (memberSelected) handlePresentModalPress();
+  }, [memberSelected]);
+
+  return {
+    visibleToast,
+    setVisibleToast,
+    handleDeleteMember,
+    bottomSheetModalRef,
+    data: queryGetMembers.data,
+    isLoading: queryGetMembers.isLoading,
+    handleGetMember,
+    memberSelected,
+  };
+};
+
+export default useMembers;
---
+++ src/features/members/hooks/useRegisterMember.ts
@@ -0,0 +1,91 @@
+import { Keyboard } from "react-native";
+import { FirebaseMembersRepository } from "../domain/repositories/FirebaseMembersRepository";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+import { FormDataRegisterMember } from "./forms/useFormRegisterMember";
+import { storage } from "global/configs/firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import * as ImagePicker from "expo-image-picker";
+import { useNavigation } from "@react-navigation/native";
+
+const useRegisterMember = ({ isEdit }: { isEdit: string }) => {
+  const repository = new FirebaseMembersRepository();
+  const queryClient = useQueryClient();
+  const [profileImage, setProfileImage] = useState("");
+  const [visibleToast, setVisibleToast] = useState(false);
+  const { goBack } = useNavigation();
+
+  const openImagePickerAsync = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      quality: 0.3,
+    });
+
+    if (result && !result.canceled && result.assets) {
+      setProfileImage(result.assets[0].uri);
+    }
+  };
+
+  const uploadImageStorage = async (name: string, memberCard: string) => {
+    const response = await fetch(profileImage);
+    const blob = await response.blob();
+    const imageRef = ref(
+      storage,
+      `component-${name}-${memberCard}/profileImage`
+    );
+
+    try {
+      const uploadTask = await uploadBytes(imageRef, blob);
+      const uri = await getDownloadURL(uploadTask.ref);
+      return uri;
+    } catch (error: any) {
+      console.log("Erro no upload de imagem", error);
+      return "";
+    }
+  };
+
+  const createMemberMutation = useMutation({
+    mutationFn: async (data: FormDataRegisterMember) => {
+      let uri = "";
+      if (profileImage)
+        uri = await uploadImageStorage(data?.name, data?.memberCard);
+      console.log({ isEdit });
+      return isEdit
+        ? repository.updateMember({ ...data, id: isEdit, profileImageUri: uri })
+        : repository.createMember({ ...data, profileImageUri: uri });
+    },
+    onSuccess: () => {
+      setVisibleToast(true);
+      queryClient.invalidateQueries({
+        queryKey: ["members"],
+      });
+      console.log(
+        isEdit
+          ? "✅ Membro editado com sucesso!"
+          : "✅ Membro criado com sucesso!"
+      );
+      setTimeout(() => {
+        goBack();
+      }, 2500);
+    },
+    onError: (erro) => console.error("[MutationRegister] ->", erro),
+  });
+
+  const onSubmit = (dataForm: FormDataRegisterMember) => {
+    Keyboard.dismiss();
+    createMemberMutation.mutate(dataForm);
+  };
+
+  return {
+    onSubmit,
+    visibleToast,
+    setProfileImage,
+    setVisibleToast,
+    openImagePickerAsync,
+    profileImage,
+    isLoading: createMemberMutation?.isPending,
+  };
+};
+
+export default useRegisterMember;
